7ec6f96b36dcbf8865c6b1d0fd68cf00d7cfbcf1
BiGMaX20 <67732777+BiGMaX20@users.noreply.github.com>
2020-07-08 20:15:45 +0300

    Add files via upload

---
+++ ApproximDots2/ApproximDots2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ApproximDots2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ApproximButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ApproximButton
+            // 
+            this.ApproximButton.Location = new System.Drawing.Point(22, 284);
+            this.ApproximButton.Name = "ApproximButton";
+            this.ApproximButton.Size = new System.Drawing.Size(114, 23);
+            this.ApproximButton.TabIndex = 0;
+            this.ApproximButton.Text = "Аппроксимировать";
+            this.ApproximButton.UseVisualStyleBackColor = true;
+            this.ApproximButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(171, 284);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(118, 23);
+            this.ClearButton.TabIndex = 1;
+            this.ClearButton.Text = "Очистить данные";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(254, 223);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 583);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ApproximButton);
+            this.Name = "Form1";
+            this.Text = "Аппроксимация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ApproximButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ApproximDots2/ApproximDots2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApproximDots2
+{
+    public partial class Form1 : Form
+    {
+        private List<float> X, Y;
+        private float Summarxy, Summmarx2, Summmarx, Summary, Summarsr;
+        private float a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            X.Clear(); Y.Clear();
+            dataGridView1.Columns.Clear(); dataGridView1.ColumnCount = 2;
+            label1.Text = null;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X = new List<float>();
+            Y = new List<float>();
+
+            dataGridView1.ColumnCount = 2;
+            label1.Text = null;
+            textBox1.Text = "0";
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                X.Add((float)Convert.ToDouble(dataGridView1.Rows[i].Cells[0].Value));
+                Y.Add((float)Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value));
+
+            }
+            for (int i = 0; i < X.Count; i++)
+            {
+                Summmarx += X[i];
+                Summary += Y[i];
+                Summarxy += X[i] * Y[i];
+                Summmarx2 += X[i] * X[i];
+            }
+            a = (float)(X.Count * Summarxy - Summmarx * Summary) / (float)(X.Count * Summmarx2 - Summmarx * Summmarx);
+            b = (float)(Summary - a * Summmarx) / (float)X.Count;
+            for (int i = 0; i < X.Count; i++)
+            {
+                Summarsr += ((Y[i] - (a * X[i] + b)) * (Y[i] - (a * X[i] + b)));
+            }
+            textBox1.Text = Convert.ToString(Summarsr);
+            label1.Text = "y = " + a + "x + " + b;
+        }
+    }
+}
